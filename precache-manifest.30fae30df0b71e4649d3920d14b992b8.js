20a3521d3cfe3343f91cc67d10bc3ad6495e21a4
Mukul Srivastav <mukulsrivastav551@gmail.com>
2023-01-28 22:32:25 +0530

    Updates

---
+++ precache-manifest.30fae30df0b71e4649d3920d14b992b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0896985b4d159b6b7c0850f741ff6a36",
+    "url": "/Job-listing-app/index.html"
+  },
+  {
+    "revision": "ba7b4ced31fbde7001f9",
+    "url": "/Job-listing-app/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "3bb49d18c5eafc7e1a1d",
+    "url": "/Job-listing-app/static/js/2.71c0f001.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Job-listing-app/static/js/2.71c0f001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba7b4ced31fbde7001f9",
+    "url": "/Job-listing-app/static/js/main.eccce91f.chunk.js"
+  },
+  {
+    "revision": "cdfdef0aa53788342c5c",
+    "url": "/Job-listing-app/static/js/runtime-main.59479658.js"
+  }
+]);
